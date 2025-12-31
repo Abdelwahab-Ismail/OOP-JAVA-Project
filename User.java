0661f593e7c69698e1b279876bf526a7560290c1
Abdelwahab-Ismail <abdelwahabismail21@istanbularel.edu.tr>
2026-01-01 00:21:18 +0300

    adding user class controls users login

---
+++ User.java
@@ -0,0 +1,12 @@
+public class User {
+
+    int id;
+    String username;
+    String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
