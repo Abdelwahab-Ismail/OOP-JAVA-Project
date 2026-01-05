3f0b872e9dc0ab6c993fc3e7cfbb1e4e0bc42d42
Abdelwahab-Ismail <abdelwahabismail21@istanbularel.edu.tr>
2026-01-05 15:17:08 +0300

    Implement TimedTask using inheritance from Task

--- TimedTask.java
+++ TimedTask.java
@@ -1,8 +1,41 @@
+import java.time.LocalDate;
+
 public class TimedTask extends Task {
 
-    public TimedTask(int id, int userId, String title, Deadline deadline,
-    Priority priority, String note) {
+    private LocalDate deadline;
+    private Priority priority;
+    private String note;
 
-        super(id, userId, title, deadline, priority, note);
+    public TimedTask(int id, int userId, String title,
+    LocalDate deadline, Priority priority, String note) {
+        super(id, userId, title);
+        this.deadline = deadline;
+        this.priority = (priority == null) ? Priority.LOW : priority;
+        this.note = (note == null || note.isEmpty()) ? "NONE" : note;
+    }
+
+    @Override
+    public boolean isUpcoming() {
+        return deadline != null &&
+        deadline.isAfter(LocalDate.now()) &&
+        !isCompleted();
+    }
+
+    @Override
+    public String toCSV() {
+        return getId() + "," + getUserId() + "," + getTitle() + "," +
+        deadline + "," + priority + "," + note + "," + isCompleted();
+    }
+
+    @Override
+    public void display() {
+        System.out.println(
+            "ID: " + getId() +
+            " | Title: " + getTitle() +
+            " | Deadline: " + deadline +
+            " | Priority: " + priority +
+            " | Note: " + note +
+            " | Completed: " + isCompleted()
+        );
     }
 }
