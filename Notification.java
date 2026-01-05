8d29176851b7e6b6ffa89817afd96babe7712c2c
Abdelwahab-Ismail <abdelwahabismail21@istanbularel.edu.tr>
2026-01-06 00:01:15 +0300

    improve the reminder

--- Notification.java
+++ Notification.java
@@ -1,6 +1,17 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Notification {
 
-    public static void remind(String taskName) {
-        System.out.println("Reminder: Task \"" + taskName + "\" is coming soon");
+    public static void showReminder(LocalDate deadline) {
+        if (deadline == null) {
+            return;
+        }
+
+        long daysLeft = ChronoUnit.DAYS.between(LocalDate.now(), deadline);
+
+        if (daysLeft >= 0) {
+            System.out.println(">> Reminder: " + daysLeft + " day(s) left for this task");
+        }
     }
 }
